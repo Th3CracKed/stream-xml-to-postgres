507dbcb66ca48c3561726446efdd6ce173bd597d
Driowya Abdelghafour <abdodriowya@gmail.com>
2021-12-22 00:19:22 +0100

    adapt script to use attributes

--- src/index.ts
+++ src/index.ts
@@ -12,16 +12,16 @@ import { from as copyFrom } from 'pg-copy-streams';
 
 import { createStream } from 'sax';
 
-async function streamData({ table, fields }: { table: string, fields: string[] }) {
+async function streamData({ table, fields }: { table: string, fields: { value: string, mandatory?: true, bool?: true }[] }) {
     const startTime = process.hrtime();
     pool.connect((err: Error, client: PoolClient, release: (release?: any) => void) => {
-        const fieldsStringified = fields.reduce((acc, field, curentIndex) => acc + field.toLowerCase() + (curentIndex !== fields.length - 1 ? ',' : ''), '');
+        const fieldsStringified = fields.reduce((acc, field, currentIndex) => acc + field.value.toLowerCase() + (currentIndex !== fields.length - 1 ? ',' : ''), '');
         const query = `COPY ${table} (${fieldsStringified}) FROM STDIN WITH DELIMITER ',' NULL AS ''`;
         // fs.writeFile(`query.txt`, `${query}`, (err) => {
         //     if (err) throw err;
         // });
         const dbStream = client.query(copyFrom(query))
-            .on('error', error => {
+            .on('error', (error: any) => {
                 console.log('error', error);
                 release();
                 fs.appendFile(`stream_error_${table}.json`, `saxStream error | nbOfProcessedRows = ${nbOfProcessedRows} | ${JSON.stringify({ error })}`, (err) => {
@@ -39,42 +39,67 @@ async function streamData({ table, fields }: { table: string, fields: string[] }
         const saxStream = createStream(true, {});
         let nbErrors: number = 0;
         let nbOfProcessedRows = 0;
+        let attributes: { [key: string]: string } = {};
         const readStream = fs.createReadStream(`data/${table}.xml`);
         saxStream.on('end', () => dbStream.end());
         saxStream.on("error", (error: Error) => console.log(error));
         dbStream.on("drain", () => {
             // console.log('drained');
             readStream.resume();
-        })
-        saxStream.on("opentag", async function (node) {
+        });
+        saxStream.on('attribute', (attribute) => {
+            attributes[attribute.name] = attribute.value;
+        });
+        saxStream.on('closetag', () => {
+            // console.log({ attributes })
             nbOfProcessedRows++;
-            if (node.name !== 'row') {
-                return;
-            }
             if (nbOfProcessedRows % 10000 === 0) {
                 console.log(`Processing the ${nbOfProcessedRows} row, nb of nbErrors ${nbErrors}/${nbOfProcessedRows}`);
             }
-            if (!node.attributes) {
+            if (!attributes) {
                 nbErrors++;
-                fs.appendFile(`errors_${table}.json`, `${{ nbOfProcessedRows }} | Attributes undefined | ${JSON.stringify({ node })}`, (err) => {
+                fs.appendFile(`errors_${table}.json`, `${{ nbOfProcessedRows }} | Attributes undefined `, (err) => {
                     if (err) throw err;
                 });
                 return;
             }
             const regexDelimiter = new RegExp(',', 'g');
-
+            let ignoreRow = false;
             const row = fields.reduce((acc, field, currentIndex) => {
                 const isLastField = currentIndex === fields.length - 1;
                 const delimiter = isLastField ? '' : ',';
-                const attributeValue = node.attributes[field];
-                // if(!attributeValue && isLastField){
-                //     return acc + ' '+ delimiter;
-                // }else 
-                if (!attributeValue) {
-                    return acc + delimiter;
+                if (field.bool) {
+                    const attributeValue = attributes[field.value] === 'False' ? 0 : 1;
+                    if (!attributeValue && field.mandatory) {
+                        ignoreRow = true;
+                    }
+                    if (!attributeValue) {
+                        return acc + delimiter;
+                    }
+                    return acc + attributeValue + delimiter;
+                } else {
+                    const attributeValue = attributes[field.value];
+                    // if(!attributeValue && isLastField){
+                    //     return acc + ' '+ delimiter;
+                    // }else 
+                    if (!attributeValue && field.mandatory) {
+                        ignoreRow = true;
+                    }
+                    if (!attributeValue) {
+                        return acc + delimiter;
+                    }
+                    return acc + encodeURI((<string>attributeValue).replace(regexDelimiter, ' ')) + delimiter;
                 }
-                return acc + encodeURI((<string>attributeValue).replace(regexDelimiter, ' ')) + delimiter;
+
             }, '');
+            // console.log({ row });
+            if (ignoreRow) {
+                fs.appendFile(`errors_${table}.json`, `${{ nbOfProcessedRows }} | Attributes undefined `, (err) => {
+                    if (err) throw err;
+                });
+                // console.log('Ignore row');
+                return;
+            }
             // console.log("saxStream", node);
             try {
                 // readStream.pause();
@@ -90,30 +115,93 @@ async function streamData({ table, fields }: { table: string, fields: string[] }
                     readStream.resume();
                 }
             } catch (err) {
-                fs.appendFile(`errors_${table}.json`, `${{ nbOfProcessedRows }} | Error while inserting user ${JSON.stringify({ err })} ${JSON.stringify({ node })}`, (err) => {
+                fs.appendFile(`errors_${table}.json`, `${{ nbOfProcessedRows }} | Error while inserting user ${JSON.stringify({ err })}`, (err) => {
                     if (err) throw err;
                 });
                 nbErrors++;
                 // console.log('user insertion error', JSON.stringify({ err }));   
             }
+            attributes = {};
+            // console.log('closetag');
         });
         console.log(`Parsing ${table}.xml as a steam`);
         readStream.pipe(saxStream, { end: true });
     });
 }
+(async function () {
+    // streamData({ table: 'Comments', fields: [{ value: 'Id', mandatory: true }, { value: 'PostId', }, { value: 'Score', }, { value: 'Text', }, { value: 'CreationDate', }, { value: 'UserId', }, { value: 'ContentLicense', }, { value: 'UserDisplayName' }] })
+    //     .catch((e) => {
+    //         fs.appendFile(`Comments_logs.txt`, `${JSON.stringify({ err: e })}`, (err) => {
+    //             if (err) throw err;
+    //         });
+    //     })
+    //     .finally(async () => {
+    //         // disconnect pg
+    //     });
 
-streamData({ table: 'Comments', fields: ['Id', 'PostId', 'Score', 'Text', 'CreationDate', 'UserId', 'ContentLicense', 'UserDisplayName'] })
-    .catch((e) => {
-        throw e
+    // streamData({ table: 'PostLinks', fields: [{ value: 'Id', mandatory: true }, { value: 'CreationDate' }, { value: 'PostId' }, { value: 'LinkTypeId' }] })
+    //     .catch((e) => {
+    //         fs.appendFile(`PostLinks_logs.txt`, `${JSON.stringify({ err: e })}`, (err) => {
+    //             if (err) throw err;
+    //         });
+    //     })
+    //     .finally(async () => {
+    //         // disconnect pg
+    //     })
+    // streamData({ table: 'Badges', fields: [{ value: 'Id', mandatory: true }, { value: 'UserId' }, { value: 'Name' }, { value: 'Date' }, { value: 'Class' }, { value: 'TagBased', bool: true }] })
+    //     .catch((e) => {
+    //         fs.appendFile(`Badges_logs.txt`, `${JSON.stringify({ err: e })}`, (err) => {
+    //             if (err) throw err;
+    //         });
+    //     })
+    //     .finally(async () => {
+    //         // disconnect pg
+    //     })
+    // streamData({
+    //     table: 'Users', fields: [{ value: 'Id', mandatory: true }, { value: 'Reputation' }, { value: 'CreationDate' }, { value: 'DisplayName' }, { value: 'Views' }, { value: 'WebsiteUrl' },
+    //     { value: 'Location' }, { value: 'AboutMe' }, { value: 'Age' }, { value: 'UpVotes' }, { value: 'DownVotes' }, { value: 'EmailHash' }, { value: 'AccountId' }, { value: 'ProfileImageUrl' }
+    //     ]
+    // })
+    //     .catch((e) => {
+    //         fs.appendFile(`Users_logs.txt`, `${JSON.stringify({ err: e })}`, (err) => {
+    //             if (err) throw err;
+    //         });
+    //     })
+    //     .finally(async () => {
+    //         // disconnect pg
+    //     })
+    // streamData({
+    //     table: 'Posts', fields: [{ value: 'Id', mandatory: true }, { value: 'PostTypeId' }, { value: 'AcceptedAnswerId' }, { value: 'ParentId' }, { value: 'Score' }, { value: 'ViewCount' },
+    //     { value: 'Body' }, { value: 'OwnerUserId' }, { value: 'LastEditorUserId' }, { value: 'LastEditDate' }, { value: 'LastActivityDate' }, { value: 'Title' }, { value: 'Tags' }, { value: 'AnswerCount' },
+    //     { value: 'CommentCount' }, { value: 'FavoriteCount' }, { value: 'CreationDate' }, { value: 'CommunityOwnedDate' },
+    //     { value: 'ContentLicense' }, { value: 'LastEditorDisplayName' }, { value: 'OwnerDisplayName' }
+    //     ]
+    // })
+    //     .catch((e) => {
+    //         fs.appendFile(`Users_logs.txt`, `${JSON.stringify({ err: e })}`, (err) => {
+    //             if (err) throw err;
+    //         });
+    //     })
+    //     .finally(async () => {
+    //         // disconnect pg
+    //     })
+    streamData({
+        table: 'Votes', fields: [{ value: 'Id', mandatory: true }, { value: 'PostId' }, { value: 'VoteTypeId' }, { value: 'CreationDate' }]
     })
-    .finally(async () => {
-        // disconnect pg
-    })
+        .catch((e) => {
+            fs.appendFile(`Users_logs.txt`, `${JSON.stringify({ err: e })}`, (err) => {
+                if (err) throw err;
+            });
+        })
+        .finally(async () => {
+            // disconnect pg
+        })
+}())
 
-streamData({ table: 'Votes', fields: ['Id', 'PostId', 'VoteTypeId', 'CreationDate'] })
-    .catch((e) => {
-        throw e
-    })
-    .finally(async () => {
-        // disconnect pg
-    })
+// streamData({ table: 'Votes', fields: ['Id', 'PostId', 'VoteTypeId', 'CreationDate'] })
+//     .catch((e) => {
+//         throw e
+//     })
+//     .finally(async () => {
+//         // disconnect pg
+//     })
